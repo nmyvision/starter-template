0a5d246ab47ef04d6208ce246a497f1c2ac73d8c
nmyvision <mgambill@nmyvision.com>
2018-03-05 23:39:01 -0500

    Update meta.js

--- meta.js
+++ meta.js
@@ -20,6 +20,12 @@ module.exports = {
       'type': 'string',
       'message': 'Author'
     },
+    description: {
+      'type': 'number',
+      'required': false,
+      'message': 'Port #',
+      'default': 3335
+    },    
   },
   completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
 };
