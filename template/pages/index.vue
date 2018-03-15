5094ca3b54cb443fff5b3a69464d26dab8091a8b
M. Gambill <git@nmyvision.com>
2018-03-14 21:09:23 -0400

    update to use pug format

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -1,25 +1,12 @@
-<template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        {{ name }}
-      </h1>
-      <h2 class="subtitle">
-        {{ description }}
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+section.container
+  div
+    app-logo
+      h1.title {{ name }}
+      h2.subtitle {{ description }}
+      .links
+        a.button--green(href='https://nuxtjs.org/', target='_blank') Documentation
+        a.button--grey(href='https://github.com/nuxt/nuxt.js', target='_blank') GitHub
 </template>
 
 <script>
